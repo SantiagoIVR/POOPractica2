bea297e6e6907c3a6f119d592c1dc759ff97e557
Santiago Izquierdo Varela <santiago.izquierdo2203@uppuebla.edu.mx>
2024-05-14 12:03:30 -0600

    Se modifico el Main y se agrego DatosPeronales y Coleccion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+//        DatosPersonales obj = new DatosPersonales("Santiago","Izquierdo Varela");
+//        System.out.println(obj.getNombre());
+
+        Coleccion objc = new Coleccion();
+
+        DatosPersonales dp1 = new DatosPersonales("Santiago","Izquierdo Varela");
+        objc.add(new DatosPersonales("Santiago","Izquierdo Varela"));
+        objc.add(new DatosPersonales("Santiago","Izquierdo Varela"));
+        objc.add(new DatosPersonales("Valeria","Izquierdo Varela"));
+        objc.add(new DatosPersonales("Santiago","Izquierdo Varela"));
+
+//        objc.show();
+//        objc.show2();
+        objc.buscar("Valeria");
 
     }
 }
